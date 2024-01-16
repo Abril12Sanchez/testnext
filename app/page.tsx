d733e4255cca6f1a2642e2aa80cef1a5ab58680a
Abril Sánchez <doloresabrilsanchez@gmail.com>
2024-01-16 11:25:30 -0600

    AGREGE IMAGENES

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -11,7 +12,18 @@ export default function Home() {
         <li>
           <Link href="/pages/test">Test</Link>
         </li>
+
+        <li>
+          <Link href="/">Contacto</Link>
+        </li>
+
       </ul>
+      <h1>Fotitos</h1>
+      <Image src="/ima1.jpg" alt="Imagen 1" width={200} height={200}/>
+      <Image src="/ima2.jpg" alt="Imagen 1" width={200} height={200}/>
+      <Image src="/ima3.jpg" alt="Imagen 1" width={200} height={200}/>
+      
+
     </main>
   );
 }
